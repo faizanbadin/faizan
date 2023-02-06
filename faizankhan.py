f3664a415a1daa7132877e04effc5fe833f88d13
faizanbadin <124583184+faizanbadin@users.noreply.github.com>
2023-02-06 15:44:50 +0000

    Pending changes exported from your codespace

---
+++ faizankhan.py
@@ -0,0 +1,48 @@
+q1 = "Q 1: Who developed python programming language?"
+q2 = "Q 2 : which type of programming does python support?"
+q3 = "Q 3 : is python case sensitive when dealing with identifiers?"
+q4 = "Q 4 : what is the correct extension of python file?"
+q5 = "Q 5 : Is Python code is compiled or interpreted?"
+q6 = "Q 6 : Name a few blocks of code use to define in Python language."
+q7 = "Q 7: State a character used to give single line comments in Python. "
+q8 = "Q 8: Mention function which can help us to find the version of python that"
+q9 = "Q 9 : What does PIP stand for Python?"
+q10 = "Q 10 : Mention few built in function in python."
+q11 = "Q 11: What is maximum possible length of an identifier in Python?"
+q12 = "Q 12 : What are the benefits of using Python?"
+a1 = "GUIDO VAN ROSSUM "
+a2 = ") Python supports four main programming paradigms: imperative, functional, procedural, and object-oriented."
+a3 = "Yes, Python is a case-sensitive language,"
+a4 = "'. py' is the correct extension of the Python file."
+a5 = "Python is an interpreted language, which means the source code of a Python program is converted into bytecode that is then executed by the Python virtual machine."
+a6 = "The following are blocks: a module, a function body, and a class definition. Each command typed interactively is a block. A script file (a file given as standard input to the interpreter or specified as a command line argument to the interpreter) is a code block"
+a7 = "Python offers a pretty simple and straightforward way to write a comment. We can write a single-line comment by adding a single # character before any statement or line of code."
+a8 = "Explanation: The function sys. version can help us to find the version of python that we are currently working on"
+a9 = "Pip is a recursive acronym that can stand for either 'Pip Installs Packages' or 'Pip Installs Python'. Alternatively, pip stands for 'preferred installer program'"
+a10 = "Python Built-in Functions, 'print' ,'type', 'input' ,'abs', 'pow', 'dir', 'sorted', 'max'"
+a11 = " An identifier can have a maximum length of 79 characters in Python. Python is one of the most popular programming languages."
+a12 = "Top Reasons to Learn Python 1 Data science. 2 Scientific and mathematical computing. 3 Web development. 4 Finance and trading. 5 System automation and administration. 6 Computer graphics. 7 Basic game development. 8 Security and penetration testing."
+print(q1)
+print("Ans "+a1)
+print(q2)
+print("Ans "+a2)
+print(q3)
+print("Ans "+a3)
+print(q4)
+print("Ans "+a4)
+print(q5)
+print("Ans "+a5)
+print(q6)
+print("Ans "+a6)
+print(q7)
+print("Ans "+a7)
+print(q8)
+print("Ans "+a8)
+print(q9)
+print("Ans "+a9)
+print(q10)
+print("Ans "+a10)
+print(q11)
+print("Ans "+a11)
+print(q12)
+print("Ans "+a12)
